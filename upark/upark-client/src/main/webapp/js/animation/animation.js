95a1babaf29818b1c3bbf592efe76e84ed64b2d4
XShan <sx_890511@163.com>
2014-10-03 22:47:57 -0400

    UI v1.0
    必须在服务器下运行

--- upark/upark-client/src/main/webapp/js/animation/animation.js
+++ upark/upark-client/src/main/webapp/js/animation/animation.js
@@ -1,18 +1,18 @@
+
 $(document).ready(function(){
-	$("#link-news").mouseover(function(){
-			// div.stop(true,true);
-			// div.css("height","100px");
-			// div.css("width","100px");
-			// div.css("display","none");
-			// div=$("#flyanimate1");
-			// div.css("display","block");
-			// $("#flydown1").css("display","inline-block");
+	$(".link-sidebar").mouseover(function(){
+		$("#extended-page").stop();
 		$("#extended-page").css("display","inline-block");
-  		$("#extended-page").animate({width:'70%'},"slow",function(){});
+  		$("#extended-page").css("width","0px").animate({width:'70%'},"slow",function(){});
 	
 	});	
 	$("#sidebar"&&"#extended-page").mouseleave(function(){
+		$("#extended-page").stop(true,true);
 		$("#extended-page").css("display","none");
 		$("#extended-page").css("width","0px");
-	})
+	});
+
+	$("#sign-in").click(function(){
+		$("#modal-sign-in").modal().css({'margin-top': function(){return ($(this).height()/2-120);}});
+	});
 });
